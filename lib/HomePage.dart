3bbe3826057dc37e827132ed852c8b7b361b6579
peyton232 <peyton.t131@gmail.com>
2021-05-01 00:12:57 -0500

    basic song loop complete

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:ru_hacks/CustomWidgets/PianoKeys.dart';
 import 'package:ru_hacks/PianoPage.dart';
+import 'package:ru_hacks/database/database.dart';
+import 'package:ru_hacks/functions/songLoop.dart';
 import 'constants.dart';
+import 'package:ru_hacks/data/globals.dart' as globals;
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,16 +18,30 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Piano Game"),
       ),
-      body: ElevatedButton(
+      body:
+      ElevatedButton(
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) {
-                return PianoPage();
-              },
-            ),
-          );
+          getSongs();
+          Future.delayed(const Duration(milliseconds: 500), () {
+
+            songLoop(globals.easySongs.first);
+
+            setState(() {
+              // Here you can write your code for open new view
+            });
+
+          });
+
+
+          // Navigator.push(
+          //   context,
+          //   MaterialPageRoute(
+          //     builder: (context) {
+          //       return PianoPage();
+          //     },
+          //   ),
+          // );
+
         },
       ),
     );
