3b3cdbecdd1897a3c0486a247a592784a0b86944
peyton232 <peyton.t131@gmail.com>
2021-05-01 01:11:10 -0500

    making files able to merge

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ru_hacks/CustomWidgets/CustomMenuButton.dart';
 import 'package:ru_hacks/CustomWidgets/PianoKeys.dart';
+import 'package:ru_hacks/Pages/FreestylePage.dart';
 import 'package:ru_hacks/Pages/PianoPage.dart';
-import 'package:ru_hacks/database/database.dart';
-import 'package:ru_hacks/functions/songLoop.dart';
-import 'constants.dart';
-import 'package:ru_hacks/data/globals.dart' as globals;
+import 'package:ru_hacks/constants.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,34 +14,87 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text("Piano Game"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body:
-      ElevatedButton(
-        onPressed: () {
-          getSongs();
-          Future.delayed(const Duration(milliseconds: 500), () {
+      body: Stack(
+        children: <Widget>[
+          //Background
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment
+                    .bottomRight, // 10% of the width, so there are ten blinds.
+                colors: <Color>[
+                  Colors.purple[900],
+                  kRoyalBlue,
+                ], // one color to another
+              ),
+            ),
+          ),
 
-            songLoop(globals.easySongs.first);
+          //UI Elements
+          Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: Center(
+                    child: Text("(App Logo)"),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Center(
+                    child: Text("(App Name)"),
+                  ),
+                ),
+              ),
 
-            setState(() {
-              // Here you can write your code for open new view
-            });
-
-          });
-
-
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(
-          //     builder: (context) {
-          //       return PianoPage();
-          //     },
-          //   ),
-          // );
-
-        },
+              //Buttons
+              Expanded(
+                child: Container(
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        CustomMenuButton(
+                          buttonLabel: "Freestyle",
+                          whenPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return FreestylePage();
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                        CustomMenuButton(
+                          buttonLabel: "Levels",
+                          whenPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return PianoPage();
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
